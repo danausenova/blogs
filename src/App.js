ff448e8407e073dd0e380ad8f3fdf1e53b212e3f
danausenova <aidana99969@gmail.com>
2023-07-09 22:36:57 +0600

    update crd on db.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import MainRoute from "./routes/MainRoute";
+import { useEffect, useState } from "react";
+import axios from "axios";
+const API = "http://localhost:8000/posts";
 function App() {
+  const [blogs, setBlogs] = useState([]);
+  async function getBlogs() {
+    const { data } = await axios(API);
+    setBlogs(data);
+  }
+  useEffect(() => {
+    getBlogs();
+  }, []);
+  async function deleteBlog(id) {
+    await axios.delete(`${API}/${id}`);
+    getBlogs();
+  }
+  async function addBlog(title, body) {
+    const newBlog = { userId: 1, title, body };
+    await axios.post(API, newBlog);
+    getBlogs();
+  }
   return (
     <div>
-      <MainRoute />
+      <MainRoute blogs={blogs} deleteBlog={deleteBlog} addBlog={addBlog} />
     </div>
   );
 }
--- src/routes/MainRoute.jsx
+++ src/routes/MainRoute.jsx
@@ -7,12 +7,15 @@ import AboutUs from "../pages/AboutUs";
 import Contacts from "../pages/Contacts";
 import Favorite from "../pages/Favorite";
 
-const MainRoute = () => {
+const MainRoute = ({ blogs, deleteBlog, addBlog }) => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/blog" element={<AddBlog />} />
+        <Route
+          path="/"
+          element={<MainPage blogs={blogs} deleteBlog={deleteBlog} />}
+        />
+        <Route path="/blog" element={<AddBlog addBlog={addBlog} />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/favorite" element={<Favorite />} />
